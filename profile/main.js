1a77d16e99cd30f070f340127a22931442fb5bc2
eunhuychon <108158535+eunhuychon@users.noreply.github.com>
2022-06-26 14:41:19 +0900

    Add files via upload

---
+++ profile/main.js
@@ -0,0 +1,4 @@
+let div = document.getElementById("profile");
+
+loadNewElement( div, "div", "{index}/profile/style.html", true) ;
+loadNewElement( div, "div", "{index}/profile/profile.html", true) ;
