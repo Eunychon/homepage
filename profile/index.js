467f61dab427517226976a559b6dac45a4b631d8
eunhuychon <108158535+eunhuychon@users.noreply.github.com>
2022-06-26 11:43:41 +0900

    Add files via upload

---
+++ profile/index.js
@@ -0,0 +1,10 @@
+loadDoc(document.head,"./../head/head.html",true );
+var body = document.body ;
+loadDoc( body, "./../header/header.html", false, function(){
+ loadNewElement( body, "script", "./../header/header.js", true ) ;
+}) ;
+loadNewElement( body, "div", "main.html", true, function(){
+ loadNewElement( body, "script", "main.js", true, function(){
+  loadNewElement( body, "div", "./../footer/footer.html", true ) ;
+ }) ;
+}) ;
