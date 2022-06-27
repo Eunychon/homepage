c82ecd34faa259549263eb9b14cd67cdbdb31c16
eunhuychon <108158535+eunhuychon@users.noreply.github.com>
2022-06-27 14:25:33 +0900

    Add files via upload

--- dir.py
+++ dir.py
@@ -12,31 +12,53 @@ for url in urls:
  url = url.replace('\\', '/')
  arr = url.split('/')
  name = arr[-1]
- dir = arr[-2]
- data = {"name": name}
+ dir = '/'.join(arr[0:-1])
+ data = {"name": name, "path":dir}
  try:
   json_obj[dir]. append(data)
  except:
   json_obj. update({dir:[]})
   json_obj[dir]. append(data)
 
+"""
 str=json.dumps(json_obj)
 print(str)
-
-f = open('dir.json','w')
-f.write(str)
-f.close()
+"""
 
 ########################
 
 token=os.environ['GITHUB_TOKEN']
 rep_name=os.environ['REPOSITORY'].split('/')[-1]
-filename='assets/test.txt'
-data='blabla'
-#print([token, rep_name, filename, data])
 
 g = Github(token)
 rep = g.get_user().get_repo(rep_name)
-files = rep.get_contents(filename);
 
+"""
+filename='assets/list.json'
+data='blabla'
+#print([token, rep_name, filename, data])
+
+files = rep.get_contents(filename);
 rep.update_file(filename,'json commit',data,files.sha)
+
+rep.create_file(filename,'json commit',data)
+"""
+
+for dir, data in json_obj.items():
+ filename=dir+'/list.json'
+ data=json.dumps(data)
+ if not os.path.exists(filename):
+  print('create_file')
+  print(filename)
+  print(data)
+  rep.create_file(filename,'json commit',data)
+ else:
+  f=open(filename,'r')
+  f_data=f.read()
+  f.close()
+  if data!=f_data:
+   print('update_file')
+   print(filename)
+   print(data)
+   files = rep.get_contents(filename);
+   rep.update_file(filename,'json commit',data,files.sha)
