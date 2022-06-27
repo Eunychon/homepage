e8c3b0ee3b9b7b3bb6a3d2ceafae25796f3bdc47
eunhuychon <108158535+eunhuychon@users.noreply.github.com>
2022-06-27 11:48:49 +0900

    Add files via upload

--- dir.py
+++ dir.py
@@ -13,11 +13,12 @@ for url in urls:
  arr = url.split('/')
  name = arr[-1]
  dir = arr[-2]
+ data = {"name": name}
  try:
-  json_obj[dir]. append(name)
+  json_obj[dir]. append(data)
  except:
   json_obj. update({dir:[]})
-  json_obj[dir]. append(name)
+  json_obj[dir]. append(data)
 
 str=json.dumps(json_obj)
 print(str)
@@ -27,11 +28,12 @@ f.write(str)
 f.close()
 
 ########################
+
 token=os.environ['GITHUB_TOKEN']
 rep_name=os.environ['REPOSITORY'].split('/')[-1]
 filename='/assets/test.txt'
 data='blabla'
-print([token, rep_name, filename, data])
+#print([token, rep_name, filename, data])
 
 g = Github(token)
 rep = g.get_user().get_repo(rep_name)
